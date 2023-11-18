2b13625be694befae68125f2960f4e4582d99020
canhhuynh2k2 <canhhuynh2002@gmail.com>
2023-11-18 22:35:18 +0700

    update recommendation service

--- target/generated-sources/annotations/com/example/stepcounter/service/stepcounter/StepCounterMapperImpl.java
+++ target/generated-sources/annotations/com/example/stepcounter/service/stepcounter/StepCounterMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-17T17:31:58+0700",
+    date = "2023-11-18T22:33:41+0700",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
--- target/generated-sources/annotations/com/example/stepcounter/service/user/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/stepcounter/service/user/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-11-17T17:31:59+0700",
+    date = "2023-11-18T22:33:41+0700",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.8 (Oracle Corporation)"
 )
 @Component
